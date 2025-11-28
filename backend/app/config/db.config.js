bb8f010d5abd86a91447cb79796d78a52653f4e1
Vaddi Harshitha <107269932+VaddiHarshitha@users.noreply.github.com>
2025-11-28 14:31:54 +0530

    Update db.config.js

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   HOST: process.env.DB_HOST || "localhost",
   PORT: process.env.DB_PORT || 27017,
-  USER: process.env.DB_USER || "root",
+  USER: process.env.DB_USER || "admin",
   PASSWORD: process.env.DB_PASSWORD || "admin123",
   DB: process.env.DB_NAME || "mean_crud_db"
 };
