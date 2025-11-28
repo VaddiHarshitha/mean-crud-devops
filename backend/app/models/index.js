e4bbaf68fa7cf52b34c403e3fa4c1c80282c5faa
Vaddi Harshitha <107269932+VaddiHarshitha@users.noreply.github.com>
2025-11-28 14:30:19 +0530

    Update index.js

--- backend/app/models/index.js
+++ backend/app/models/index.js
@@ -5,7 +5,23 @@ mongoose.Promise = global.Promise;
 
 const db = {};
 db.mongoose = mongoose;
-db.url = dbConfig.url;
+
+// Build MongoDB connection URL
+console.log("DB Config:", {
+  HOST: dbConfig.HOST,
+  PORT: dbConfig.PORT,
+  USER: dbConfig.USER,
+  DB: dbConfig.DB
+});
+
+if (dbConfig.USER && dbConfig.PASSWORD) {
+  db.url = `mongodb://${dbConfig.USER}:${dbConfig.PASSWORD}@${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}?authSource=admin`;
+} else {
+  db.url = `mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`;
+}
+
+console.log("MongoDB URL constructed:", db.url ? "✓" : "✗");
+
 db.tutorials = require("./tutorial.model.js")(mongoose);
 
 module.exports = db;
